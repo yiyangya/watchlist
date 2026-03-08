bf0224dd9f359418a53cd45b40944604f9935502
yiyangya <yiyanyao@bu.edu>
2026-03-07 21:21:48 -0500

    Add static files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from markupsafe import escape
 from flask import url_for
 
 
-name = 'Grey Li'
+name = 'Ian Yao'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
@@ -28,7 +28,7 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    print(url_for('index'))  # 生成 hello 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
